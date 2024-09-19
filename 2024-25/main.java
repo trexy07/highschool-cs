a5b36ea521a7749ca17439a88967f1ac8949d210
Trevin <trex.trevin@gmail.com>
2024-09-19 11:02:20 -0500

    ran a code question to find that no answer is corect

--- 2024-25/main.java
+++ 2024-25/main.java
@@ -2,30 +2,52 @@ import java.io.IOException;
 
 public class main {
     public static void main(String[] args) {
-        try {
-            // Set terminal width and height
-            String command = "resize";//&& stty rows 33 && stty cols 1000";
-            ProcessBuilder processBuilder = new ProcessBuilder("bash", "-c", command);
-            System.out.println(processBuilder.inheritIO().start().waitFor());
+        String message = new String("AP Practice");
+        String note = new String("AP Practice");
+        String memo = new String("memo");
+        int i = 5;
 
+        if (message.equals(note) && !message.equals("memo"))
+        {
+            message = note;
 
-            // String command2 = "tput lines";
-            // ProcessBuilder processBuilder2 = new ProcessBuilder("bash", "-c", command);
-            // System.out.println(processBuilder2.inheritIO().start().waitFor());
-            
-            System.out.println("Terminal size changed successfully.");
-            
-            for (int i = 0; i < 10; i++) {
-                for (int j = 0; j < 10; j++) {
-                    System.out.print(j);
-                    }
+            if (message == note && message.length() > i)
+            {
+            i = 3;
+            memo = message.substring(i);
             }
-            System.out.print("over");
-
-        } catch (IOException | InterruptedException e) {
-            System.out.println("broken");
-
-            e.printStackTrace();
         }
+        System.out.println(message == note && message == memo);
+        System.out.println(message.equals(note) && message.equals(memo));
+        System.out.println(message == note && memo.equals("AP Practice"));
+        System.out.println(message != note || message == memo);
+        System.out.println(message.equals(memo) || memo.equals(note));
+        System.out.println(message+","+note+","+memo);
+        
+        // try {
+        //     // Set terminal width and height
+        //     String command = "resize";//&& stty rows 33 && stty cols 1000";
+        //     ProcessBuilder processBuilder = new ProcessBuilder("bash", "-c", command);
+        //     System.out.println(processBuilder.inheritIO().start().waitFor());
+
+
+        //     // String command2 = "tput lines";
+        //     // ProcessBuilder processBuilder2 = new ProcessBuilder("bash", "-c", command);
+        //     // System.out.println(processBuilder2.inheritIO().start().waitFor());
+            
+        //     System.out.println("Terminal size changed successfully.");
+            
+        //     for (int i = 0; i < 10; i++) {
+        //         for (int j = 0; j < 10; j++) {
+        //             System.out.print(j);
+        //             }
+        //     }
+        //     System.out.print("over");
+
+        // } catch (IOException | InterruptedException e) {
+        //     System.out.println("broken");
+
+        //     e.printStackTrace();
+        // }
     }
 }
